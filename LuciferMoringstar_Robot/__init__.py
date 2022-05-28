096668b9cc21c4e1f8c52256522ed57e123ee8d1
Roahitvijay <100504788+Roahitvijay@users.noreply.github.com>
2022-05-28 19:04:44 +0530

    Add files via upload

---
+++ LuciferMoringstar_Robot/__init__.py
@@ -0,0 +1,44 @@
+from .Utils import (
+   get_filter_results,
+   get_file_details,
+   is_subscribed,
+   get_poster,
+   Media
+)
+from .Channel import (
+   RATING,
+   GENRES
+)
+
+HELP = """
+Basic Commads
+
+/start :- check am Alive
+
+Bot Owner Only
+/broadcast Replay Any Message or Media
+/stats user Status
+/ban_user  click ban_user more info
+/unban_user click unban_user more info
+/banned_users Banned User Details
+/total How Many Files Added In Database
+/logger  Get Logs
+/delete Delete File From Database
+/index To index the files
+
+"""
+
+ABOUT = """
+➥ 🤖 My Name: [Star Anime](http://t.me/staranime1bot)
+    
+➥ 🧑 My Creator : [R0ahit](https://t.me/R0ahit)
+
+➥ Language : Python3
+
+➥ 👥 My Anime group search : [Animepiratesgroup](https://t.me/animegroup99)
+
+➥ 📺 My Anime channel : [Animepirateschannel](https://t.me/animepirateschannel)
+
+➥ 🏷️ My Updates : [Animechannel90](https://t.me/Animechannel90)
+   
+➥ 🗣️ My Support : [Any Doubt](t.me/R0ahit)"""
